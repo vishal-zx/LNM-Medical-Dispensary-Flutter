110bde201269f2255576a26c9c25c7352e0ac56e
vishal-zx <19ucs053@lnmiit.ac.in>
2021-12-26 16:20:47 +0530

    book appointment ui partially done

--- lib/pages/patient/book_appointment.dart
+++ lib/pages/patient/book_appointment.dart
@@ -12,6 +12,21 @@ class BookAppointment extends StatefulWidget {
 }
 
 class _BookAppointmentState extends State<BookAppointment> {
+  final formKey = GlobalKey<FormState>(); 
+
+  List<DropdownMenuItem<String>> get dropdownItems{
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(child: Text("USA"), value: "USA"),
+      const DropdownMenuItem(child: Text("Canada"), value: "Canada"),
+      const DropdownMenuItem(child: Text("Brazil"), value: "Brazil"),
+      const DropdownMenuItem(child: Text("England"), value: "England"),
+    ];
+    return menuItems;
+  }
+  
+  String selectedValue = "USA";
+  String reason = "";
+  bool isAppointUrgent = false;
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +81,125 @@ class _BookAppointmentState extends State<BookAppointment> {
                             padding: EdgeInsets.only(left:mqw*0.02, right:mqw*0.07),
                             width: mqw*0.84,
                             height: mqh*0.755,
-                            child: Container(),
+                            child: Form(
+                              key: formKey,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children:[
+                                    Text(
+                                      "Doctor",
+                                      style:TextStyle(
+                                        fontSize: mqh*0.035,
+                                        color: Colors.black87,
+                                      )
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.015
+                                    ),
+                                    DropdownButtonFormField(
+                                      decoration: InputDecoration(
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.redAccent.shade100, width: 2),
+                                          borderRadius: BorderRadius.circular(mqh*0.02),
+                                        ),
+                                        filled: true,
+                                        fillColor: Colors.redAccent[100],
+                                      ),
+                                      dropdownColor: Colors.redAccent.shade100,
+                                      value: selectedValue,
+                                      onChanged: (String? newValue) {
+                                        setState(() {
+                                          selectedValue = newValue!;
+                                        });
+                                      },
+                                      items: dropdownItems
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.06
+                                    ),
+                                    Text(
+                                      "Preferred Time",
+                                      style:TextStyle(
+                                        fontSize: mqh*0.035,
+                                        color: Colors.black87,
+                                      )
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.01
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.06
+                                    ),
+                                    Text(
+                                      "Reason",
+                                      style:TextStyle(
+                                        fontSize: mqh*0.035,
+                                        color: Colors.black87,
+                                      )
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.01
+                                    ),
+                                    TextFormField(
+                                      decoration: const InputDecoration(
+                                        hintText: "Enter reason for appointment",
+                                      ),
+                                      maxLines: null,
+                                      onChanged:(value){
+                                        reason = value;
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.06
+                                    ),
+                                    Text(
+                                      "Type of Appointment",
+                                      style:TextStyle(
+                                        fontSize: mqh*0.035,
+                                        color: Colors.black87,
+                                      )
+                                    ),
+                                    SizedBox(
+                                      height:mqh*0.01
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Normal",
+                                          style:TextStyle(
+                                            fontSize: mqh*0.023,
+                                            color: Colors.black87,
+                                          )
+                                        ),
+                                        Switch(
+                                          value: isAppointUrgent, 
+                                          onChanged: (bool value){
+                                            setState((){
+                                              isAppointUrgent = value;
+                                            });
+                                          }
+                                        ),
+                                        Text(
+                                          "Emergency",
+                                          style:TextStyle(
+                                            fontSize: mqh*0.023,
+                                            color: Colors.black87,
+                                          )
+                                        ),
+                                      ],
+                                    ),
+                                    TextButton(onPressed: (){
+
+                                    }, 
+                                    child: const Text("ok"))
+                                  ]
+                                )
+                              ),
+                            ),
                           ),
                         ),
                       ],
